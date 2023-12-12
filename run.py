476c43e0b97b87ba28a07a5ebe26bcbd02dc0bdd
panteL <sebi.stahl1811@live.com>
2023-12-12 19:17:33 +0100

    feat: :zap: Crawl custom website and create new recipes
    Its now possible to crawl custom websites and create new recipes based on the crawl results. You can choose if you want to enable/disable Tags

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from functions import auth, validate, requests
+from functions import *
 from colorama import Fore, Style
 
 # Load Auth-Token and API-URL or prompt for user auth input if not available
@@ -19,9 +19,9 @@ if not api_url:
 
 # Prompt for script selection input
 selected_script = validate.user_input(
-    """Which script do you want to run? 
-    1 - Create a new recipe from URL (bulk or single) with/without Tags
-    2 - Coming soon ...
+    """\nWhich script do you want to run? 
+    1 - Bulk Create new recipes from URLs (with/without Tags)
+    2 - Crawl custom website and create new recipes based on crawl results (with/without Tags)
     Select your option (1-2): """, valid_options=["1", "2"])
 
 if selected_script == "1":
@@ -46,4 +46,19 @@ if selected_script == "1":
                 f"\n{Fore.RED}Invalid URL: {url} (URL should start with 'http://' or 'https://'){Style.RESET_ALL}")
 
 elif selected_script == "2":
-    print("coming soon...")
+   # Prompt for category input
+    category = validate.user_input(
+        '\nWhich category should I crawl (fruhstuck, abendessen, mittagessen, dessert): ', valid_options=['fruhstuck', 'abendessen', 'mittagessen', 'dessert'])
+
+    # Prompt for include tags input
+    include_tags = validate.user_input(
+        '\nDo you want to import original keywords as tags? (true, false): ', valid_options=['true', 'false'])
+
+    base_url = "https://www.kitchenstories.com/de/kategorien/"
+
+    # Extract recipe links
+    links = crawler(base_url, category, '/rezepte/')
+
+    # Loop through each URL and create POST
+    for link in links:
+        post_recipes_create_url(link, include_tags, token, api_url)
