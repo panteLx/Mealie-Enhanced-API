8d03f80408a8bbe611acd2edb101cc73d4953f56
panteL <sebi.stahl1811@live.com>
2023-12-12 22:07:00 +0100

    chore: Changed user_input attribute is_token
    Changed user_input attribute is_token to is_text to make it reusable within other functions.

--- functions/validate.py
+++ functions/validate.py
@@ -4,7 +4,7 @@ from colorama import Fore, Style
 # Function to validate user input
 
 
-def user_input(prompt, valid_options=None, is_url=False, is_token=False):
+def user_input(prompt, valid_options=None, is_url=False, is_text=False):
     # Get user input and remove leading/trailing whitespaces
     user_input = input(prompt).strip()
 
@@ -15,8 +15,8 @@ def user_input(prompt, valid_options=None, is_url=False, is_token=False):
         sys.exit()
 
     # Check if the input is a token and is not empty
-    if is_token and not user_input:
-        print(f"\n{Fore.RED}Token must not be empty{Style.RESET_ALL}")
+    if is_text and not user_input:
+        print(f"\n{Fore.RED}Field must not be empty{Style.RESET_ALL}")
         sys.exit()
 
     # Check if the input is within the valid options (if provided)
