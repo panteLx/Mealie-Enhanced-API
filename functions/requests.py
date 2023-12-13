5525a4ebfba8df1cf5c260456e40336227a84119
panteL <sebi.stahl1811@live.com>
2023-12-13 09:10:32 +0100

    feat: ✨ Check for API connection and auth data
    The script will now check for an established API connection. It will also validate the provided token.

--- functions/requests.py
+++ functions/requests.py
@@ -5,26 +5,49 @@ from colorama import Fore, Style
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin
 
+
+# Function to send a GET request
+def get_user_self(api_url, token):
+    # Check connection and authentication data
+    print(f"\n{Fore.BLUE}Checking connection and auth data...{Style.RESET_ALL}")
+
+    headers = {"Content-Type": "application/json", "Authorization": f"Bearer {token}"}
+
+    # Send a GET request to get data
+    response = requests.get(f"{api_url}/api/users/self", headers=headers)
+
+    # Print information about the response
+    if response.status_code == 200:
+        print(
+            f"\n{Fore.GREEN}Connection established! Auth data success! - Status Code: {response.status_code}{Style.RESET_ALL}"
+        )
+    else:
+        print(
+            f"\n{Fore.RED}Error while connecting to your API! - Status Code: {response.status_code}, Response: {response.text}{Style.RESET_ALL}"
+        )
+        sys.exit()
+
+
 # Function to send a POST request to create a URL
-
-
 def post_recipes_create_url(url, include_tags, token, api_url):
     # Prepare data and headers for the POST request
-    data = {'url': url, 'include_tags': include_tags}
-    headers = {'Content-Type': 'application/json',
-               'Authorization': f'Bearer {token}'}
+    data = {"url": url, "include_tags": include_tags}
+    headers = {"Content-Type": "application/json", "Authorization": f"Bearer {token}"}
 
     # Send a POST request to create a new recipe
     response = requests.post(
-        f"{api_url}/api/recipes/create-url", data=json.dumps(data), headers=headers)
+        f"{api_url}/api/recipes/create-url", data=json.dumps(data), headers=headers
+    )
 
     # Print information about the response
     if response.status_code == 201:
         print(
-            f"\n{Fore.GREEN}Created recipe - URL: {url}, Include Tags: {include_tags}, Status Code: {response.status_code}, Response: {response.text}{Style.RESET_ALL}")
+            f"\n{Fore.GREEN}Created recipe - URL: {url}, Include Tags: {include_tags}, Status Code: {response.status_code}, Response: {response.text}{Style.RESET_ALL}"
+        )
     else:
         print(
-            f"\n{Fore.RED}Parse Error - URL: {url}, Include Tags: {include_tags}, Status Code: {response.status_code}, Response: {response.text}{Style.RESET_ALL}")
+            f"\n{Fore.RED}Parse Error - URL: {url}, Include Tags: {include_tags}, Status Code: {response.status_code}, Response: {response.text}{Style.RESET_ALL}"
+        )
 
     # Extract the recipe slug from the response
     slug = response.text.strip('"')
@@ -32,39 +55,44 @@ def post_recipes_create_url(url, include_tags, token, api_url):
     # Check if the slug is a digit (indicating a duplicate)
     if slug[-1].isdigit():
         # Prompt the user to delete the duplicate recipe
-        user_input_delete = input(
-            f"\nDuplicate found! Do you want to delete it? (yes, no or blank): ").strip().lower()
-        if user_input_delete == 'yes':
+        user_input_delete = (
+            input(f"\nDuplicate found! Do you want to delete it? (yes, no or blank): ")
+            .strip()
+            .lower()
+        )
+        if user_input_delete == "yes":
             # Call the delete_recipes function to delete the duplicate recipe
             delete_recipes(api_url, headers, slug)
 
+
 # Function to send a DELETE request to delete a recipe
-
-
 def delete_recipes(api_url, headers, slug):
     # Send a DELETE request to delete the specified recipe
-    response = requests.delete(
-        f"{api_url}/api/recipes/{slug}", headers=headers)
+    response = requests.delete(f"{api_url}/api/recipes/{slug}", headers=headers)
 
     # Print information about the deletion response
     print(
-        f"\n{Fore.GREEN}Deleted {slug} - Status Code: {response.status_code}{Style.RESET_ALL}")
+        f"\n{Fore.GREEN}Deleted {slug} - Status Code: {response.status_code}{Style.RESET_ALL}"
+    )
 
 
 # Function to crawl external websites for recipes
-
 def crawler(url, keyword):
     response = requests.get(url)
 
     if response.status_code == 200:
-        soup = BeautifulSoup(response.text, 'html.parser')
+        soup = BeautifulSoup(response.text, "html.parser")
 
-        # Find all links
-        links = {urljoin(url, a['href']) for a in soup.find_all(
-            'a', href=True) if keyword in a['href']}
+        # Find all links containing the keyword
+        links = {
+            urljoin(url, a["href"])
+            for a in soup.find_all("a", href=True)
+            if keyword in a["href"]
+        }
 
         return list(links)
     else:
         print(
-            f"\n{Fore.RED}Error while crawling site! - Status Code: {response.status_code}{Style.RESET_ALL}")
+            f"\n{Fore.RED}Error while crawling site! - Status Code: {response.status_code}{Style.RESET_ALL}"
+        )
         sys.exit()
--- run.py
+++ run.py
@@ -7,31 +7,43 @@ from colorama import Fore, Style
 token, api_url = auth.load_auth_input_from_file()
 if not token:
     # Prompt the user to enter API-Token
-    token = validate.user_input(
-        'Enter your API-Token: ', is_text=True).strip()
+    token = validate.user_input("Enter your API-Token: ", is_text=True).strip()
     auth.save_auth_input_to_file(token, api_url)
 
 if not api_url:
     # Prompt the user to enter API-URL
     api_url = validate.user_input(
-        '\nEnter your API-URL (without path - e.g. http://mealie.dev:9925): ', is_url=True).strip()
+        "\nEnter your API-URL (without path - e.g. http://mealie.dev:9925): ",
+        is_url=True,
+    ).strip()
     auth.save_auth_input_to_file(token, api_url)
+
+# Check connection and auth data
+get_user_self(api_url, token)
 
 # Prompt for script selection input
 selected_script = validate.user_input(
     f"""\nWhich script do you want to run? 
     1 - Bulk Create new recipes from URLs (with/without Tags)
     2 - {Fore.RED}ALPHA{Style.RESET_ALL} - Crawl custom website and create new recipes based on crawl results (with/without Tags)
-    Select your option (1-2): """, valid_options=["1", "2"])
+    Select your option (1-2): """,
+    valid_options=["1", "2"],
+)
 
+# Bulk Create new recipes from URLs (with/without Tags)
 if selected_script == "1":
     # Prompt for include tags input
     include_tags = validate.user_input(
-        '\nDo you want to import original keywords as tags? (true, false): ', valid_options=['true', 'false'])
+        "\nDo you want to import original keywords as tags? (true, false): ",
+        valid_options=["true", "false"],
+    )
 
     # Prompt the user to input URLs separated by commas
-    user_input_urls = validate.user_input(
-        '\nEnter URLs separated by commas: ', is_url=True).strip().split(',')
+    user_input_urls = (
+        validate.user_input("\nEnter URLs separated by commas: ", is_url=True)
+        .strip()
+        .split(",")
+    )
 
     # Loop through each URL
     for url in user_input_urls:
@@ -39,29 +51,39 @@ if selected_script == "1":
         if url.strip().startswith(("http://", "https://")):
             # Send a POST request for each URL
             print(f"{Fore.BLUE}\nChecking {url}{Style.RESET_ALL}")
-            requests.post_recipes_create_url(
-                url.strip(), include_tags, token, api_url)
+            requests.post_recipes_create_url(url.strip(), include_tags, token, api_url)
         else:
             print(
-                f"\n{Fore.RED}Invalid URL: {url} (URL should start with 'http://' or 'https://'){Style.RESET_ALL}")
+                f"\n{Fore.RED}Invalid URL: {url} (URL should start with 'http://' or 'https://'){Style.RESET_ALL}"
+            )
 
+
+# Crawl custom website and create new recipes based on crawl results (with/without Tags)
 elif selected_script == "2":
-    print(f"\n{Fore.RED}ALPHA - Crawler has a moderate chance to find bad URLs that can't be parsed! Mealie API will throw an error (400). However this won't break anything. {Style.RESET_ALL}")
+    print(
+        f"\n{Fore.RED}ALPHA - Crawler has a moderate chance to find bad URLs that can't be parsed! Mealie API will throw an error (400). However this won't break anything. {Style.RESET_ALL}"
+    )
 
-   # Prompt for url input
+    # Prompt for url input
     url = validate.user_input(
-        "\nInput your external recipe URL to crawl (e.g. https://www.kitchenstories.com/en/categories/breakfast): ", is_url=True).strip()
+        "\nInput your external recipe URL to crawl (e.g. https://www.kitchenstories.com/en/categories/breakfast): ",
+        is_url=True,
+    ).strip()
 
     # Prompt for keyword input
     keyword = validate.user_input(
-        '\nEnter crawler keyword - Crawler will search for all results within your external recipe URL containing this keyword (e.g. "recipes"): ', is_text=True)
+        '\nEnter crawler keyword - Crawler will search for all results within your external recipe URL containing this keyword (e.g. "recipes"): ',
+        is_text=True,
+    )
 
     # Prompt for include tags input
     include_tags = validate.user_input(
-        '\nDo you want to import original keywords as tags? (true, false): ', valid_options=['true', 'false'])
+        "\nDo you want to import original keywords as tags? (true, false): ",
+        valid_options=["true", "false"],
+    )
 
     # Extract recipe links
-    links = crawler(url, f'/{keyword}/')
+    links = crawler(url, f"/{keyword}/")
 
     # Loop through each URL and create POST
     for link in links:
