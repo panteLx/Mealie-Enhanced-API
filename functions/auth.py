7790de25a2483cea1bf543244129f68953ab56b0
panteL <sebi.stahl1811@live.com>
2023-12-13 09:07:56 +0100

    style: 🎨 Formatting changes
    Perform formatting changes to insure better readability

--- functions/auth.py
+++ functions/auth.py
@@ -1,25 +1,28 @@
 import os
 import json
 
-CONFIG_DIR = 'config'
+CONFIG_DIR = "config"
 
 # Ensure directories exist
 os.makedirs(CONFIG_DIR, exist_ok=True)
 
 
 # Function to save authentication input to a file
-def save_auth_input_to_file(token, api_url, file_path=os.path.join(CONFIG_DIR, 'auth.json')):
-    with open(file_path, 'w') as file:
+def save_auth_input_to_file(
+    token, api_url, file_path=os.path.join(CONFIG_DIR, "auth.json")
+):
+    with open(file_path, "w") as file:
         # Write authentication information as JSON to the file
-        json.dump({'token': token, 'api_url': api_url}, file)
+        json.dump({"token": token, "api_url": api_url}, file)
+
 
 # Function to load authentication input from a file
 
 
-def load_auth_input_from_file(file_path=os.path.join(CONFIG_DIR, 'auth.json')):
+def load_auth_input_from_file(file_path=os.path.join(CONFIG_DIR, "auth.json")):
     if os.path.exists(file_path):
-        with open(file_path, 'r') as file:
+        with open(file_path, "r") as file:
             # Load JSON from the file
             data = json.load(file)
-        return data.get('token'), data.get('api_url')
+        return data.get("token"), data.get("api_url")
     return None, None
--- functions/validate.py
+++ functions/validate.py
@@ -1,9 +1,8 @@
 import sys
 from colorama import Fore, Style
 
+
 # Function to validate user input
-
-
 def user_input(prompt, valid_options=None, is_url=False, is_text=False):
     # Get user input and remove leading/trailing whitespaces
     user_input = input(prompt).strip()
@@ -11,7 +10,8 @@ def user_input(prompt, valid_options=None, is_url=False, is_text=False):
     # Check if input is a URL and starts with "http://" or "https://://"
     if is_url and not user_input.startswith(("http://", "https://")):
         print(
-            f"\n{Fore.RED}Invalid URL: {user_input} (URL should start with 'http://' or 'https://'){Style.RESET_ALL}")
+            f"\n{Fore.RED}Invalid URL: {user_input} (URL should start with 'http://' or 'https://'){Style.RESET_ALL}"
+        )
         sys.exit()
 
     # Check if the input is a token and is not empty
@@ -22,7 +22,8 @@ def user_input(prompt, valid_options=None, is_url=False, is_text=False):
     # Check if the input is within the valid options (if provided)
     if valid_options and user_input not in valid_options:
         print(
-            f"\n{Fore.RED}Invalid input. Must be one of {valid_options}{Style.RESET_ALL}")
+            f"\n{Fore.RED}Invalid input. Must be one of {valid_options}{Style.RESET_ALL}"
+        )
         sys.exit()
 
     # Return the validated user input
